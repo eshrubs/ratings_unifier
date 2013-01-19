c2fb27293a62c9ee0e9cfc53a33f6e6e51385028
Evan Shrubsole <eshr@spotify.com>
2013-01-19 21:59:57 +0100

    move to main.py
    Change-Id: I8a340ca7ef13f3f81abd5f080cddb01ac3c30f4a

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import argparse
+
+# import progressbar
+
+from rt_scraper import config, rt_scraper
+
+def main():
+    parser = argparse.ArgumentParser(description='Scrape ratings')
+
+    parser.add_argument('-c', '--csv-output',
+                        help='Filename for csv output')
+    parser.add_argument('--rt-user-id',
+                        default=config.RT_USER_ID,
+                        help='Your RT user number')
+    parser.add_argument('-s',
+                        '--site',
+                        choices=['rt'],
+                        default='rt')
+
+    args = parser.parse_args()
+
+    if (args.site == 'rt'):
+        with open(args.csv_output, 'w') as f:
+            rt_scraper.scrape(args.rt_user_id, outfile=f)
+    else:
+        raise Exception("Didn't scrape")
+
+
+if __name__ == '__main__':
+    main()
