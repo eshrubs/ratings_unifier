bc25730903fffad57c634afdac4482936769d114
Evan Shrubsole <evanshrubsole@gmail.com>
2013-01-27 11:33:54 +0100

    rating and login works. Used mobile login because it works better

--- imdb/utils.py
+++ imdb/utils.py
@@ -11,15 +11,18 @@ import cookielib
 
 from bs4 import BeautifulSoup
 
-cookiejar = cookielib.CookieJar()
+cookiejar = cookielib.LWPCookieJar()
 url_opener = urllib2.build_opener(
         urllib2.HTTPCookieProcessor(cookiejar))
-url_opener.add_header = [('User-Agent', 'ratings-unifier/1')]
+url_opener.add_header = [('User-Agent',
+'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 
 def login(login, password):
-    url = "https://secure.imdb.com/oauth/login"
+    # Use mobile login because it actually works
+    url = 'https://secure.imdb.com/oauth/m_login?origpath=/'
     values = {
+        # '49e6c': 'cad9',
         'login': login,
         'password': password,
     }
@@ -28,7 +31,7 @@ def login(login, password):
         data = urllib.urlencode(values)
         request = urllib2.Request(url, data)
         response = url_opener.open(request)
-        response_data = response.read()
+        return response
     except urllib2.HTTPError, ex:
         sys.stderr.write('Request failed: %d, %s', ex.code, response_data)
         raise
@@ -44,7 +47,6 @@ def search(query):
     sys.stderr.write('%s\n' % url)
     request = urllib2.Request(url)
     response = url_opener.open(request)
-    response_data = response.read()
     return response_data
 
 
@@ -72,17 +74,39 @@ def try_get_search_page(movie, year=None):
     return top_a['href']
 
 
-def rate(movie_id, rating):
-    url = 'http://www.imdb.com/ratings/_ajax'
-    values = {
-        'tconst': movie_id,
-        'csrf': 'f04f',
-        'rating': rating,
-        'auth': [c.value for c in cookiejar if c.name == 'uu'][0]
-    }
+def get_title_page(movie_id):
+    url = "http://www.imdb.com/title/{0}".format(movie_id)
+    sys.stderr.write('Loading title page: %s\n' % url)
+    request = urllib2.Request(url)
+    response = url_opener.open(request)
+    return response
 
-    data = urllib.urlencode(values)
+def get_rating_params(movie_id, new_rating):
+    html = get_title_page(movie_id).read()
+    soup = BeautifulSoup(html)
+
+    rating_div = soup.find('div', attrs={'class', 'rating'})
+
+    auth = rating_div['data-auth']
+
+    idlist = rating_div['id'].split('|')
+    tconst = idlist[0]
+    tracking_tag = idlist[-1]
+
+    return {'tconst': tconst,
+            'rating': new_rating,
+            'auth': auth,
+            'tracking_tag': tracking_tag
+            }
+
+def rate(movie_id, rating):
+    url = 'http://www.imdb.com/ratings/_ajax/title'
+
+    rating_params = get_rating_params(movie_id, rating)
+    data = urllib.urlencode(rating_params)
     request = urllib2.Request(url, data)
+    sys.stderr.write('Posting ratings to %s\n' % url)
     response = url_opener.open(request)
     response_data = response.read()
+    return response_data
 
